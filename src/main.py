f01ce321378e2b3a7f73286a42afda20e4cd08c3
robfowler4 <rfowler97@gmail.com>
2022-04-01 19:12:22 +0000

    update

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todo
 #from models import Person
 
 app = Flask(__name__)
@@ -38,6 +38,77 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+
+@app.route('/todo', methods=['GET'])
+def get_todo():
+
+    todo_query = Todo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), todo_query))
+
+    return jsonify(all_todos), 200
+
+
+
+@app.route('/todo', methods=['POST'])
+def post_todo():
+
+    body = request.get_json()
+
+    todo1 = Todo(label = body['label'], done = body['done'])
+    db.session.add(todo1)
+    db.session.commit()
+
+    todo_query = Todo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), todo_query))
+
+    return jsonify(all_todos), 200
+
+
+
+@app.route('/todo', methods=['PUT'])
+def put_todo():
+    
+    body = request.get_json()
+
+    todo_id = Todo.query.get(body['id'])
+    if todo_id is None:
+        raise APIException('User not found', status_code=404)
+
+    if "label" in body:
+        todo_id.label = body["label"]
+    if "done" in body:
+        todo_id.done = body["done"]
+    db.session.commit()
+
+    todo_query = Todo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), todo_query))
+
+    return jsonify(all_todos), 200
+
+
+
+
+@app.route('/todo/<int:id>', methods=['DELETE'])
+def delete_todo(id):
+
+    body = request.get_json()
+
+    todo1 = Todo.query.get(id)
+    if todo1 is None:
+        raise APIException('User not found', status_code=404)
+    db.session.delete(todo1)
+    db.session.commit()
+
+    todo_query = Todo.query.all()
+    all_todos = list(map(lambda x: x.serialize(), todo_query))
+
+    return jsonify(all_todos), 200
+
+
+
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
